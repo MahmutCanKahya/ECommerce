93a470c8336330ceb4c1291ae6ced73d1650238b
MahmutCanKahya <mahmutcankahya07@gmail.com>
2021-01-01 17:58:46 +0300

    Add project files.

---
+++ ECommerce.Admin/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using ECommerce.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        UserViewModel tempUser = new UserViewModel() { Password = "123456", UserName = "kahya" };
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserViewModel user)
+        {
+            if (user.UserName==tempUser.UserName && user.Password == tempUser.Password)
+            {
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+       
+    }
+}
